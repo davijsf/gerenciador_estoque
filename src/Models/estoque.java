0ce3ffb3a685b388d565569376dadde28532ef35
carlosMateus <carlosmateusts73@gmail.com>
2025-11-27 19:03:16 -0300

    Add getters and setters for estoque fields

--- src/Models/estoque.java
+++ src/Models/estoque.java
@@ -8,5 +8,45 @@ public class estoque {
     private int quantidade;
     private String tipo_estoque;
     private Data data_recebimento;
+
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTipo_estoque() {
+        return tipo_estoque;
+    }
+
+    public void setTipo_estoque(String tipo_estoque) {
+        this.tipo_estoque = tipo_estoque;
+    }
+
+    public Data getData_recebimento() {
+        return data_recebimento;
+    }
+
+    public void setData_recebimento(Data data_recebimento) {
+        this.data_recebimento = data_recebimento;
+    }
 }
