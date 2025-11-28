ce626e0906cf981b5ed5f47e565cd8c01b00293c
MateusDv27 <carlosmateusgames604@gmail.com>
2025-11-27 21:52:56 -0300

    estoque e movimentação feito

---
+++ src/Models/estoque.java
@@ -0,0 +1,12 @@
+package Models;
+
+import javax.xml.crypto.Data;
+
+public class estoque {
+    private int id;
+    private int id_produto;
+    private int quantidade;
+    private String tipo_estoque;
+    private Data data_recebimento;
+    
+}
